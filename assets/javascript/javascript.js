6ba94ca31d79422ca5341a76920302074d92c79d
aaa_alex <alexwu7583@gmail.com>
2019-09-07 23:15:10 +1000

    cleaning and commenting code

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,35 +1,18 @@
 let arrFoods = ['pizza', 'pie', 'cheese', 'hamburger', 'fries', 'soda', 'coffee', 'steak', 'salad', 'soup'];
+
+//generate a base set of buttons from the arrFoods array
+renderPage(arrFoods);
 function renderPage(arr) {
     for (let i = 0; i < arr.length; i++) {
         prependButton(arr[i]);
     }
 }
-renderPage(arrFoods);
 
-function prependButton(inputString) {
-    let button = $('<button>');
-    button.attr('class', 'btn btn-primary m-1');
-    button.attr('data-searchterm', inputString);
-    inputString = inputString.charAt(0).toUpperCase() + inputString.substring(1, inputString.length);
-    button.html(inputString);
-    $('#containerButtons').prepend(button);
-    button.on('click', retreiveGifFirstTime);
-}
-
-
-
-
-
-
-
-
-
-
+//click event for creating new buttons
 $('#formButtonSubmit').on('click', submitFunction);
-
 function submitFunction(e) {
     e.preventDefault();
-    
+
     let inputString = $('#formInputFood').val().trim();
     $('#formInputFood').val('');
 
@@ -45,6 +28,16 @@ function submitFunction(e) {
     }
 }
 
+// helper function to add new buttons to the button set
+function prependButton(inputString) {
+    let button = $('<button>');
+    button.attr('class', 'btn btn-primary m-1');
+    button.attr('data-searchterm', inputString);
+    inputString = inputString.charAt(0).toUpperCase() + inputString.substring(1, inputString.length);
+    button.html(inputString);
+    $('#containerButtons').prepend(button);
+    button.on('click', retrieveGifFirstTime);
+}
 
 
 
@@ -52,14 +45,23 @@ function submitFunction(e) {
 
 
 
+//variable used for "show more" functionality
+let offset = 0;
 
-
-function retreiveGifFirstTime(e) {
+//function for first iteration of retrieving gifs
+function retrieveGifFirstTime(e) {
+    offset = 0;
     $('#containerGifs').empty();
-
     let food = $(this).attr("data-searchterm");
+
+    retrieveGifAjax(food);
+}
+
+//helper function to allow offset query functionality
+function retrieveGifAjax(food) {
+
     let queryURL = "https://api.giphy.com/v1/gifs/search?q=" + food + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
-    let offset = 10;
+
     if (offset != 0) {
         queryURL += '&offset=' + offset;
     }
@@ -72,30 +74,34 @@ function retreiveGifFirstTime(e) {
         let results = response.data;
 
         for (let i = 0; i < results.length; i++) {
-            console.log(results[i]);
             let card = $('<div>');
             card.attr('class', 'col-12 col-sm-6 py-3 cardItem');
-            
-            let img = $('<img>');
 
+            //code for img
+            let img = $('<img>');
             let fixedHeightGif = results[i].images.fixed_height.url;
             let fixedHeightStill = results[i].images.fixed_height_still.url
             img.attr('src', fixedHeightStill);
             img.attr('class', 'img-fluid mb-3');
 
-            img.on('mouseenter', function() {
+            img.on('mouseenter', function () {
                 img.attr('src', fixedHeightGif);
             });
-            img.on('mouseleave', function() {
+            img.on('mouseover', function () {
+                img.attr('src', fixedHeightGif);
+            });
+            img.on('mouseleave', function () {
                 img.attr('src', fixedHeightStill);
             });
             card.append(img);
-            
+
+            //code for saving gif
             let saveButton = $('<button>');
             saveButton.attr('class', 'btn btn-success mb-3 d-block');
             saveButton.text('SAVE');
             let save = false;
-            saveButton.on('click', function(){
+
+            saveButton.on('click', function () {
                 save = !save;
                 if (save) {
                     saveButton.text('REMOVE');
@@ -108,20 +114,38 @@ function retreiveGifFirstTime(e) {
                 }
             });
             card.append(saveButton);
-            
-            let pRating = $('<p></p>');
+
+            //code for meta data text
+            let pRating = $('<p>');
             pRating.html("<b>Rating:</b> " + results[i].rating);
             card.append(pRating);
 
-            let pTitle = $('<p></p>');
+            let pTitle = $('<p>');
             pTitle.html("<b>Title:</b> " + results[i].title);
             card.append(pTitle);
 
+            //code for link to source site
+            let buttonSource = $('<a>');
+            buttonSource.attr('class', 'btn btn-secondary mt-3');
+            buttonSource.attr('href', results[i].bitly_url);
+            buttonSource.attr('target', '_blank');
+            buttonSource.html("Source");
+            card.append(buttonSource);
 
-
+            //attaching the gif card to the page
             $('#containerGifs').append(card);
         }
+
+        //code for show more button
+        let buttonShowMore = $('<button>');
+        buttonShowMore.attr('class', 'btn btn-link position-relative col-12');
+        buttonShowMore.attr('style', 'left: 50%; transform: translateX(-50%);');
+        buttonShowMore.text('Show More');
+        buttonShowMore.on('click', function() {
+            buttonShowMore.remove();
+            offset += 10;
+            retrieveGifAjax(food);
+        });
+        $('#containerGifs').append(buttonShowMore);
     });
-
 }
-
